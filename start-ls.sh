4c008067424446f6088e23b7838fbaad5ffe2003
Paul Robello <probello@xojetaviation.com>
2022-08-24 12:15:20 -0700

    now works properly for starting and stopping ls within gdc

--- start-ls.sh
+++ start-ls.sh
@@ -2,12 +2,35 @@
 # make sure windows git bash does not alter paths
 export MSYS_NO_PATHCONV=1
 
-IS_HOST_LS=$(echo "$GDC_COMPOSE_FILES" | grep -sc '-f dc-ls-host.yml')
-
 cd /root/gdc-host || exit 1
 
+if [ -r ".env-gdc" ]; then
+  echo "Loading container .env-gdc environment file"
+  source ".env-gdc"
+fi
+
+if [ -r ".env-gdc-local" ]; then
+  echo "Loading container .env-gdc-local environment file"
+  source ".env-gdc-local"
+fi
+
+if [ -r "/workspace/.env-gdc" ]; then
+  echo "Loading project .env-gdc environment file"
+  source "/workspace/.env-gdc"
+fi
+if [ -r "/workspace/.env-gdc-local" ]; then
+  echo "Loading project .env-gdc-local environment file"
+  source "/workspace/.env-gdc-local"
+fi
+
+export LS_VERSION=${LS_VERSION:='latest'}
+
+IS_HOST_LS=$(echo "$GDC_COMPOSE_FILES" | grep -sc '\-f dc-ls-host.yml')
+
+
+
 if [ "$IS_HOST_LS" = "0" ]; then
-  docker-compose up -f dc-ls-host.yml --build --force-recreate
+  docker-compose -f dc-ls-host.yml up -d --build --force-recreate
 else
-  docker-compose up -f dc-ls.yml --build --force-recreate
+  docker-compose -f dc-ls.yml up -d --build --force-recreate
 fi
--- stop-ls.sh
+++ stop-ls.sh
@@ -2,12 +2,12 @@
 # make sure windows git bash does not alter paths
 export MSYS_NO_PATHCONV=1
 
-IS_HOST_LS=$(echo "$GDC_COMPOSE_FILES" | grep -sc '-f dc-ls-host.yml')
+IS_HOST_LS=$(echo "$GDC_COMPOSE_FILES" | grep -sc '\-f dc-ls-host.yml')
 
 cd /root/gdc-host || exit 1
 
 if [ "$IS_HOST_LS" = "0" ]; then
-  docker-compose down -f dc-ls-host.yml --rmi all
+  docker-compose -f dc-ls-host.yml down
 else
-  docker-compose down -f dc-ls.yml --rmi all
+  docker-compose -f dc-ls.yml down
 fi
