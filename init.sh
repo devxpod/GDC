c26e5c5b928327c1f875f188d27ee2fda003c269
Paul Robello <probello@xojetaviation.com>
2022-09-28 15:57:40 -0700

    fix container exit

--- init.sh
+++ init.sh
@@ -22,17 +22,17 @@ if [ -n "$GDC_ENTRYPOINT" ]; then
   fi
 fi
 
-if [ -z "$GDC_RUN_MODE" ] || [ "$GDC_RUN_MODE" = "daemon" ]; then
-  echo "=============================================================================================="
-  echo "connect to container shell via docker:   docker exec -it $COMPOSE_PROJECT_NAME-dev-1 bash -l"
 
-  echo "user: root   password default unless changed is: ContainersRule"
-  if [ -n "$SSH_SERVER_PORT" ]; then
-      echo "connect to container via ssh:  ssh root@localhost -p $SSH_SERVER_PORT"
-      echo "if you get a REMOTE HOST IDENTIFICATION HAS CHANGED error use the following command to fix before connecting."
-      echo "ssh-keygen -R [localhost]:$SSH_SERVER_PORT"
-  fi
-  echo "=============================================================================================="
-  echo "sleeping forever...."
-  tail -f /dev/null 2>&1
+echo "=============================================================================================="
+echo "connect to container shell via docker:   docker exec -it $COMPOSE_PROJECT_NAME-dev-1 bash -l"
+
+echo "user: root   password default unless changed is: ContainersRule"
+if [ -n "$SSH_SERVER_PORT" ]; then
+    echo "connect to container via ssh:  ssh root@localhost -p $SSH_SERVER_PORT"
+    echo "if you get a REMOTE HOST IDENTIFICATION HAS CHANGED error use the following command to fix before connecting."
+    echo "ssh-keygen -R [localhost]:$SSH_SERVER_PORT"
 fi
+echo "=============================================================================================="
+echo "sleeping forever...."
+tail -f /dev/null 2>&1
+
