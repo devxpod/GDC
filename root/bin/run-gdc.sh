40792f3222a662e65703a9da850e43758b7eb0e5
Paul Robello <probello@xojetaviation.com>
2022-09-29 14:31:09 -0700

    pass DEVNET_NAME to sub GDC launch

--- root/bin/run-gdc.sh
+++ root/bin/run-gdc.sh
@@ -9,8 +9,11 @@ echo
 env -i bash --noprofile --norc -c \
 "FORCE_PROJECT_PATH=$P \
 NO_DEVNET_RM=$NO_DEVNET_RM \
+DEVNET_NAME=$DEVNET_NAME \
 OS=$HOST_OS \
 GDC_DIR=$GDC_DIR \
 HOME=$HOST_HOME \
 PATH=$PATH \
+USER=$USER \
+LC_CTYPE="${LC_ALL:-${LC_CTYPE:-$LANG}}" \
 run-dev-container.sh $@"
