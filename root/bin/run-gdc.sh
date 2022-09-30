f66e6534eea8b2d0e9603fbd518723b4355cd3ee
Paul Robello <probello@xojetaviation.com>
2022-09-30 09:57:26 -0700

    set NO_DEVNET_RM=yes on GDC launched within GDC set GDC_PARENT var on GDC launched from GDC

--- root/bin/run-gdc.sh
+++ root/bin/run-gdc.sh
@@ -8,12 +8,13 @@ echo "Using host project path of $P"
 echo
 env -i bash --noprofile --norc -c \
 "FORCE_PROJECT_PATH=$P \
-NO_DEVNET_RM=$NO_DEVNET_RM \
+NO_DEVNET_RM=yes \
 DEVNET_NAME=$DEVNET_NAME \
 OS=$HOST_OS \
 GDC_DIR=$GDC_DIR \
 HOME=$HOST_HOME \
 PATH=$PATH \
 USER=$USER \
-LC_CTYPE="${LC_ALL:-${LC_CTYPE:-$LANG}}" \
-run-dev-container.sh $@"
+LC_CTYPE=${LC_ALL:-${LC_CTYPE:-$LANG}} \
+GDC_PARENT=$COMPOSE_PROJECT_NAME \
+run-dev-container.sh $*"
