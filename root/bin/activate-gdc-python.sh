e617be1e699635f89e7c75fe09224f38bb0050e6
Paul Robello <probello@xojetaviation.com>
2022-10-03 15:20:52 -0700

    renamed activate-gdc-python.sh to gdc-pipeline-exec.sh updated to run commands after sourcing .bashrc in force interactive mode

--- root/bin/activate-gdc-python.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-if [ -r /usr/local/pyenv ]; then
-  echo "activating pyenv"
-  export PYENV_ROOT=/usr/local/pyenv
-  command -v pyenv > /dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-  eval "$(pyenv init -)"
-  echo "python version $(python --version)"
-fi
---
+++ root/bin/gdc-pipeline-exec.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export FORCE_INTERACTIVE=yes
+source /root/.bashrc
+
+$@
