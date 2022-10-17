2b8634c41dca54e0fb2ad4654fab08c8b72c297e
Paul Robello <probello@xojetaviation.com>
2022-10-17 12:36:23 -0700

    faster and simpler stop for ls and auth0

--- root/bin/auth0/stop-auth0.sh
+++ root/bin/auth0/stop-auth0.sh
@@ -2,43 +2,6 @@
 # make sure windows git bash does not alter paths
 export MSYS_NO_PATHCONV=1
 
-cd /root/gdc-host || exit 1
+docker rm -f "$AUTH0_CONTAINER_NAME"
 
-if [ -r ".env-gdc" ]; then
-  echo "Loading container .env-gdc environment file"
-  source ".env-gdc"
-fi
-
-if [ -r ".env-gdc-local" ]; then
-  echo "Loading container .env-gdc-local environment file"
-  source ".env-gdc-local"
-fi
-
-if [ -r "/workspace/.env-gdc" ]; then
-  echo "Loading project .env-gdc environment file"
-  source "/workspace/.env-gdc"
-fi
-if [ -r "/workspace/.env-gdc-local" ]; then
-  echo "Loading project .env-gdc-local environment file"
-  source "/workspace/.env-gdc-local"
-fi
-
-IS_HOST=$(echo "$GDC_COMPOSE_FILES" | grep -sc '\-f dc-auth0-host.yml')
-if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-  echo "Usage $0 [host|internal]"
-  echo "if no parameters are passed, then GDC launch env variables are used to automatically determine mode."
-  exit 0
-elif [ "$1" = "host" ]; then
-  IS_HOST="1"
-elif [ "$1" = "internal" ]; then
-  IS_HOST="0"
-fi
-
-if [ "$IS_HOST" = "0" ]; then
-  echo "stop-auth0.sh using container mode"
-  docker-compose -f dc-auth0.yml down
-else
-  echo "stop-auth0.sh using host mode"
-  docker-compose -f dc-auth0-host.yml down
-fi
 sleep 2
--- root/bin/ls/stop-ls.sh
+++ root/bin/ls/stop-ls.sh
@@ -2,45 +2,5 @@
 # make sure windows git bash does not alter paths
 export MSYS_NO_PATHCONV=1
 
-cd /root/gdc-host || exit 1
-
-if [ -r ".env-gdc" ]; then
-  echo "Loading container .env-gdc environment file"
-  source ".env-gdc"
-fi
-
-if [ -r ".env-gdc-local" ]; then
-  echo "Loading container .env-gdc-local environment file"
-  source ".env-gdc-local"
-fi
-
-if [ -r "/workspace/.env-gdc" ]; then
-  echo "Loading project .env-gdc environment file"
-  source "/workspace/.env-gdc"
-fi
-if [ -r "/workspace/.env-gdc-local" ]; then
-  echo "Loading project .env-gdc-local environment file"
-  source "/workspace/.env-gdc-local"
-fi
-
-export LS_VERSION=${LS_VERSION:='latest'}
-
-IS_HOST=$(echo "$GDC_COMPOSE_FILES" | grep -sc '\-f dc-ls-host.yml')
-if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-  echo "Usage $0 [host|internal]"
-  echo "if no parameters are passed, then GDC launch env variables are used to automatically determine mode."
-  exit 0
-elif [ "$1" = "host" ]; then
-  IS_HOST="1"
-elif [ "$1" = "internal" ]; then
-  IS_HOST="0"
-fi
-
-if [ "$IS_HOST" = "0" ]; then
-  echo "stop-ls.sh using container mode"
-  docker-compose -f dc-ls.yml down
-else
-  echo "stop-ls.sh using host mode"
-  docker-compose -f dc-ls-host.yml down
-fi
-sleep 4
+docker rm -f "$LS_MAIN_CONTAINER_NAME"
+sleep 2
