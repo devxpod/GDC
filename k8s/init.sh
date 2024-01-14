f78f6798fb7496b2fe51f7806cd6c70394d11754
Chad Beaulac <chad.beaulac@inclinevillagetech.com>
2024-01-14 08:43:44 -0800

    Setup localstack AWS profile based on env var

--- k8s/init.sh
+++ k8s/init.sh
@@ -2,10 +2,12 @@
 
 source /etc/term_colors.sh
 
-
 if [ -x /postStartCommand.sh ]; then
 . /postStartCommand.sh
 fi
+
+envsubst < /root/.aws/config.template > /root/.aws/config;
+
 echo "sleeping forever...."
 tail -f /dev/null 2>&1
 
